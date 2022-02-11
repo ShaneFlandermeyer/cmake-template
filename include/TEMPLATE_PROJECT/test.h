a5d44db712aec1a62a0d306edce9caea87091634
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-02-11 08:48:34 -0600

    Added install option

---
+++ include/TEMPLATE_PROJECT/test.h
@@ -0,0 +1,8 @@
+#ifndef AF460D91_5B8D_49EF_BEF7_63C00F2BF116
+#define AF460D91_5B8D_49EF_BEF7_63C00F2BF116
+
+// TODO: Remove me
+// This source file does nothing, and is just used to demonstrate adding header
+// folders to include/TEMPLATE_PROJECT/CMakeLists.txt sources
+
+#endif /* AF460D91_5B8D_49EF_BEF7_63C00F2BF116 */
---
+++ lib/test.cc
@@ -0,0 +1,3 @@
+// TODO: Remove me
+// This source file does nothing, and is just used to demonstrate adding source
+// folders to lib/CMakeLists.txt sources
